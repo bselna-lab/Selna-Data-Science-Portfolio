150a18fe4ff84b00c4e3fcbccf6c39ee2b003d71
bselna-lab <bselna@nd.edu>
2026-09-15 09:45:03 -0400

    week 4

--- week3/my_app.py
+++ week3/my_app.py
@@ -7,4 +7,8 @@ if st.button("Click me!"):
     st.write("🎉 You clicked the button! Nice work! 🚀")
 else:
     st.write("Click the button to see what happens...")
-    
+import pandas as pd
+
+st.subheader("Exploring Our Dataset")
+
+df = pd.read_csv("/Users/bridgetselna/Documents/GitHub/Selna-Data-Science-Portfolio/week3/data/sample_data.csv")
